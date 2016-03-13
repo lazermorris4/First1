6dfd3fa18c3fb160d4910795f1271e73ea1dce23
Lazer Morris <lazermorris@gmail.com>
2016-03-13 12:22:17 -0400

    MarchSunCommitt

---
+++ src/main/java/lazer/first/App.java
@@ -0,0 +1,13 @@
+package lazer.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
